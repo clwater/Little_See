979b30af3b00292a1649d51da1dc1e199b89b0ff
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-09 15:50:36 +0800

    20161009

---
+++ Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clwater.littesee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Request<String> request = NoHttp.createStringRequest(url, requestMethod);
+    }
+}
