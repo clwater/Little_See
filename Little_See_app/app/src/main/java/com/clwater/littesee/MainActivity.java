147587225d1c52cb73883cd8ef7cd314531e72ca
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-09 17:51:09 +0800

    20161009

--- Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
+++ Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
@@ -2,14 +2,47 @@ package com.clwater.littesee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG ="Little_See";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Little_See" , "Little_See");
 
-        Request<String> request = NoHttp.createStringRequest(url, requestMethod);
+        okHttp_asynchronousGet();
+    }
+
+    private void okHttp_asynchronousGet(){
+        try {
+            Log.i(TAG,"main thread id is "+Thread.currentThread().getId());
+            String url = "https://api.github.com/";
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+                    // 注：该回调是子线程，非主线程
+                    Log.i(TAG,"callback thread id is "+Thread.currentThread().getId());
+                    Log.i(TAG,response.body().string());
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
