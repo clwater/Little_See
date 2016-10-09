3be2824b17ddcedeae1f7ce7c4117ef6071e6128
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-09 19:26:58 +0800

    20161009

--- Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
+++ Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
@@ -1,5 +1,8 @@
 package com.clwater.littesee;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
@@ -7,11 +10,18 @@ import android.util.EventLog;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.clwater.littesee.Utils.EventBus.EventBus_test;
 import com.clwater.littesee.Utils.OkHttp_LS;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -19,13 +29,14 @@ import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
     private String TAG ="Little_See";
 
     @InjectView(R.id.ijimage)
-    public ImageView image;
+    public ImageView ijimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,51 @@ public class MainActivity extends AppCompatActivity {
 //                .add("password", "2")
 //                .build();
 //        Log.d("gzb" ,    OkHttp_LS.okhttp_post("http://120.27.53.146:5000/api/login" , formBody));
+        InputStream image =OkHttp_LS.okhttp_getImage("https://avatars3.githubusercontent.com/u/14257964?v=3&s=466");
+
+        saveimage(image);
+
+    }
+
+    private void saveimage(InputStream image) {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        byte[] buffer = new byte[1024];
+        int lenght = 0;
+        File f=new File("a.jpg");
+        inputstreamtofile(is, f);
+        EventBus.getDefault().post(new EventBus_test() );
+
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onShowDataEvent(EventBus_test EventBus_test) {
+        String path= "a.jpg";
+        Bitmap bm = BitmapFactory.decodeFile(path);
+        ijimage.setImageBitmap(bm);//不会变形
+
+    }
+
+    public static void inputstreamtofile(InputStream ins,File file) {
+        try {
+            OutputStream os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            ins.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onDestory(){
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+
+
     }
 
 }
--- Little_See_app/app/src/main/java/com/clwater/littesee/Utils/OkHttp_LS.java
+++ Little_See_app/app/src/main/java/com/clwater/littesee/Utils/OkHttp_LS.java
@@ -3,6 +3,8 @@ package com.clwater.littesee.Utils;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -16,7 +18,9 @@ import okhttp3.RequestBody;
 public class OkHttp_LS {
 
     private String TAG ="Little_See";
-    private static String result;
+    private static String result_get;
+    private static String result_post;
+    private static InputStream result_image;
 
     public static String okhttp_get(String url){
         try {
@@ -25,18 +29,18 @@ public class OkHttp_LS {
             client.newCall(request).enqueue(new okhttp3.Callback() {
                 @Override
                 public void onFailure(okhttp3.Call call, IOException e) {
-                    result = "ok http  get error";
+                    result_get = "ok http  get error";
                 }
                 @Override
                 public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                    result = response.body().string();
+                    result_get = response.body().string();
                 }
             });
         } catch (Exception e) {
             e.printStackTrace();
-            result = "ok http  get error";
+            result_get = "ok http  get error";
         }finally {
-            return result;
+            return result_get;
         }
     }
 
@@ -47,18 +51,41 @@ public class OkHttp_LS {
             client.newCall(request).enqueue(new okhttp3.Callback() {
                 @Override
                 public void onFailure(okhttp3.Call call, IOException e) {
-                    result = "ok http  post error";
+                    result_post = "ok http  post error";
                 }
                 @Override
                 public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                    result = response.body().string();
+                    result_post = response.body().string();
                 }
             });
         } catch (Exception e) {
             e.printStackTrace();
-            result = "ok http  post error";
+            result_post = "ok http  post error";
         }finally {
-            return result;
+            return result_post;
+        }
+    }
+
+
+    public static InputStream okhttp_getImage(String url){
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(url).build();
+            client.newCall(request).enqueue(new okhttp3.Callback() {
+                @Override
+                public void onFailure(okhttp3.Call call, IOException e) {
+                    result_image = null;
+                }
+                @Override
+                public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+                    result_image = response.body().byteStream();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            result_image = null;
+        }finally {
+            return result_image;
         }
     }
 
