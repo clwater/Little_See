df9baed273ba4aa80e041c43baf55dfc22ca296f
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-10 16:25:17 +0800

    侧边栏和标题栏相关

--- Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
+++ Little_See_app/app/src/main/java/com/clwater/littesee/MainActivity.java
@@ -3,16 +3,15 @@ package com.clwater.littesee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Window;
-import android.widget.Toast;
+
 
 
 import com.clwater.littesee.Activity.AboutActivity;
@@ -21,12 +20,15 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     private String TAG ="Little_See";
 
-
-    private DrawerLayout mDrawerLayout;
-    private NavigationView mNavigationView;
+    @InjectView(R.id.ma_layout)
+    public DrawerLayout drawerLayout;
+    @InjectView(R.id.ma_nv)
+    public NavigationView navigationView;
+    @InjectView(R.id.mv_toolbar)
+    Toolbar toolbar;
 
 
     @Override
@@ -36,61 +38,52 @@ public class MainActivity extends BaseActivity {
         ButterKnife.inject(this);
 
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
-        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+
+        initNavigationView();
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
+    }
+
+    private void initNavigationView() {
         setSupportActionBar(toolbar);
-
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu_more);
         ab.setDisplayHomeAsUpEnabled(true);
-
-        setupDrawerContent(mNavigationView);
+        setupDrawerContent(navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_zhihu:
+               // getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
+                //mToolbar.setTitle("我的动态");
+                break;
+            case R.id.nav_about:
+                startActivity(new Intent(this , AboutActivity.class));
+                break;
+        }
+
+        return true;
+    }
+
 
     private void setupDrawerContent(NavigationView navigationView)
     {
         navigationView.setNavigationItemSelectedListener(
-
                 new NavigationView.OnNavigationItemSelectedListener()
                 {
-
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem)
                     {
                         menuItem.setChecked(true);
-                        mDrawerLayout.closeDrawers();
+                        drawerLayout.closeDrawers();
                         return true;
                     }
-                });
+                }
+        );
     }
-
-
-
-
-
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.actionbar_main_activity,menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.action_bar_about:
-//                startActivity(new Intent(this , AboutActivity.class));
-//                return true;
-//
-//            case R.id.action_bar_more:
-//                Toast.makeText(MainActivity.this,"您点击了更多！", Toast.LENGTH_SHORT).show();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -107,13 +100,14 @@ public class MainActivity extends BaseActivity {
                 startActivity(new Intent(this , AboutActivity.class));
                 return true;
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true ;
             default:
                 return super.onOptionsItemSelected(item);
         }
 
     }
+
 
 }
 
