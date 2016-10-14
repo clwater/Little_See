7dda75de6897e9b33c0609e2ac10588e3ecf24d6
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-14 19:40:05 +0800

    20161014

--- Little_See_app/app/src/main/java/com/clwater/littesee/EventBus/Event_RunInFront.java
+++
@@ -1,8 +0,0 @@
-package com.clwater.littesee.EventBus;
-
-/**
- * Created by gengzhibo on 16/10/14.
- */
-
-public class Event_RunInFront {
-}
--- Little_See_app/app/src/main/java/com/clwater/littesee/Fragment/ZhiHuFragment.java
+++ Little_See_app/app/src/main/java/com/clwater/littesee/Fragment/ZhiHuFragment.java
@@ -4,29 +4,21 @@ package com.clwater.littesee.Fragment;
  * Created by gengzhibo on 16/10/10.
  */
 
+import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.transition.TransitionManager;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-import com.clwater.littesee.Activity.ZhuHuInfoActivity;
-import com.clwater.littesee.EventBus.Event_RunInBack;
-import com.clwater.littesee.EventBus.Event_RunInFront;
-import com.clwater.littesee.MainActivity;
+import com.clwater.littesee.Adapater.ListViewImageAdapter;
+import com.clwater.littesee.Utils.EventBus.Event_RunInBack;
+import com.clwater.littesee.Utils.EventBus.Event_RunInFront;
 import com.clwater.littesee.R;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
@@ -36,12 +28,12 @@ import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnClick;
 import butterknife.OnItemClick;
-import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 
 import static android.os.SystemClock.sleep;
 
@@ -58,14 +50,9 @@ public class ZhiHuFragment extends Fragment
     @InjectView(R.id.zh_top_pro_text)
     public TextView pro_text;
 
+    public static Activity activity;
 
-    private static final String[] strs = new String[] {
-    "1", "2", "3", "4", "5","6" ,"7" ,"8" ,"9" ,"0" ,
-            "1", "2", "3", "4", "5","6" ,"7" ,"8" ,"9" ,"0",
-            "1", "2", "3", "4", "5","6" ,"7" ,"8" ,"9" ,"0",
-            "1", "2", "3", "4", "5","6" ,"7" ,"8" ,"9" ,"0",
-            "1", "2", "3", "4", "5","6" ,"7" ,"8" ,"9" ,"0"
-    };
+    private boolean precess_statu = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,27 +62,41 @@ public class ZhiHuFragment extends Fragment
         ButterKnife.inject(this , view);
 
         showTopProcess();
+        precess_statu = true;
+
+
+        activity = getActivity();
+
         //main_list.setAdapter(new ArrayAdapter<String>( getActivity(), android.R.layout.simple_list_item_1, strs));
 
         ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,     Object>>();/*在数组中存放数据*/
         for(int i=0;i<10;i++)
         {
             HashMap<String, Object> map = new HashMap<String, Object>();
-            map.put("ItemImage", R.drawable.ic_menu_back);
+          //  map.put("ItemImage", R.drawable.ic_menu_back);
             map.put("ItemText", "这是第"+i+"行");
             listItem.add(map);
         }
 
-        SimpleAdapter mSimpleAdapter = new SimpleAdapter(getActivity(),listItem,//需要绑定的数据
-                R.layout.listview_main_image_title,//每一行的布局//动态数组中的数据源的键对应到定义布局的View中
-                 new String[] {"ItemImage", "ItemText"},
-                new int[] {R.id.listview_main_image,R.id.listview_main_text});
-        main_list.setAdapter(mSimpleAdapter);
+        List<Map<String, Object>> list=getData();
+        main_list.setAdapter(new ListViewImageAdapter(getActivity(), list));
 
         EventBus.getDefault().register(this);
 
         return view;
     }
+
+    public List<Map<String, Object>> getData(){
+        List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < 10; i++) {
+            Map<String, Object> map=new HashMap<String, Object>();
+          //  map.put("image", R.drawable.ic_menu_back);
+            map.put("title", "这是一个标题"+i);
+            list.add(map);
+        }
+        return list;
+    }
+
 
     private void showTopProcess() {
         progressWheel.setVisibility(View.VISIBLE);
@@ -116,6 +117,7 @@ public class ZhiHuFragment extends Fragment
         pro_text.setVisibility(View.GONE);
     }
 
+
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -124,7 +126,10 @@ public class ZhiHuFragment extends Fragment
 
     @OnItemClick(R.id.main_list)
     public void itemClick(int position , View view){
-        stopTopProsess();
+        if (precess_statu) {
+            stopTopProsess();
+            precess_statu = false;
+        }
     }
 
 
