b806164454a8a383394da467a4ef2763b683d7f6
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-18 19:34:30 +0800

    haoqixin  =-=  真难

--- Little_See_Server/little_see/HaoQiXin.py
+++ Little_See_Server/little_see/HaoQiXin.py
@@ -3,30 +3,30 @@
 
 
 from selenium import webdriver
-zhihulist = []
+haoqixinlist = []
 
 
 
 def main():
     driver = webdriver.PhantomJS(executable_path="/Users/haizhi/Desktop/Little_See/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
     #driver = webdriver.PhantomJS(executable_path="/Users/yszsyf/Desktop/android/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
-    driver.get("http://daily.zhihu.com/")
-    #print(driver.page_source)
-    storelist = driver.find_elements_by_class_name("wrap")
-    linklist = driver.find_elements_by_class_name("link-button")
-    imagelist = driver.find_elements_by_class_name("preview-image")
+    driver.get("http://www.qdaily.com/tags/29.html")
+    # print(driver.page_source)
+    storelist = driver.find_elements_by_class_name("packery-item")
+ #   linklist = driver.find_elements_by_class_name("com-grid-article")
+    #imagelist = driver.find_elements_by_class_name(" lazyloaded")
 
-    zhihu_length = len(storelist)
+    haoqixin_length = len(storelist)
 
-    for index  in range(zhihu_length):
-        zhihu = []
-        zhihu.insert(0, storelist[index].text)
-        zhihu.insert(1, linklist[index].get_attribute("href"))
-        zhihu.insert(2, imagelist[index].get_attribute("src"))
-        zhihulist.append(zhihu)
+    for index  in range(haoqixin_length):
+        haoqixin = []
+        haoqixin.insert(0, storelist[index].text)
+ #       haoqixin.insert(1, linklist[index].get_attribute("href"))
+     #   haoqixin.insert(2, imagelist[index].get_attribute("src"))
+        haoqixinlist.append(haoqixin)
 
-    for zhihu in zhihulist:
-        print(zhihu)
+    for haoqixin in haoqixinlist:
+        print(haoqixin)
 
 
 main()
