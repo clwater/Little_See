6044bc4d030d77909f51dccb9bc020b44552bcc8
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-22 22:09:16 +0800

    20161022

--- Little_See_Server/little_see/WangYi.py
+++ Little_See_Server/little_see/WangYi.py
@@ -24,23 +24,27 @@ def getIndexInfo(driver ,index):
     print("@@@@")
 
 def main():
-    driver = webdriver.PhantomJS(executable_path="/Users/haizhi/Desktop/Little_See/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
-    #driver = webdriver.PhantomJS(executable_path="/Users/yszsyf/Desktop/android/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
+    #driver = webdriver.PhantomJS(executable_path="/Users/haizhi/Desktop/Little_See/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
+    driver = webdriver.PhantomJS(executable_path="/Users/yszsyf/Desktop/android/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
     driver.get("http://news.163.com/")
     time.sleep(5)
 
     index = 1
-    while True:
-        try :
-            getIndexInfo(driver , index)
-            index = index + 1
-        except Exception :
-            index = index - 1
-            _path = "//*[@id=\"index2016_wrap\"]/div[2]/div[2]/div[3]/div[2]/div[5]/div/ul/li[1]/div/div[" + str(index) + "]/a"
-            ele = driver.find_element_by_xpath(_path)
-            ActionChains(driver).move_to_element(ele).perform()
-            time.sleep(5)
-            index = index + 1
+    index_length = 20
+    while index < index_length:
+        getIndexInfo(driver , index)
+        index = index + 1
+
+
+
+
+        # except Exception :
+        #     index = index - 1
+        #     _path = "//*[@id=\"index2016_wrap\"]/div[2]/div[2]/div[3]/div[2]/div[5]/div/ul/li[1]/div/div[" + str(index) + "]/a"
+        #     ele = driver.find_element_by_xpath(_path)
+        #     ActionChains(driver).move_to_element(ele).perform()
+        #     time.sleep(5)
+        #     index = index + 1
 
 
 
