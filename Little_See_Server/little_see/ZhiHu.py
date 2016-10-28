d4c0ee634d34f2d6382b4e46b61673396354f736
gengzhibo <gengzhibo@gengzhibo.net>
2016-10-28 22:30:24 +0800

    20161028

--- Little_See_Server/little_see/ZhiHu.py
+++ Little_See_Server/little_see/ZhiHu.py
@@ -9,7 +9,7 @@ zhihulist = []
 
 def main():
     #driver = webdriver.PhantomJS(executable_path="/Users/haizhi/Desktop/Little_See/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
-    #driver = webdriver.PhantomJS(executable_path="/Users/yszsyf/Desktop/android/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
+    driver = webdriver.PhantomJS(executable_path="/Users/yszsyf/Desktop/android/Little_See/Little_See_Server/phantomjs-2.1.1-macosx/bin/phantomjs")
     driver.get("http://daily.zhihu.com/")
     #print(driver.page_source)
     storelist = driver.find_elements_by_class_name("wrap")
