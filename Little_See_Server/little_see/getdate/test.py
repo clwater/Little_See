ef5cd898f7ad388412f472704d05703fe679b71d
gengzhibo <gengzhibo@gengzhibo.net>
2016-11-16 18:51:22 +0800

    20161116 服务器线程处理

--- Little_See_Server/little_see/getdate/test.py
+++
@@ -1,4 +0,0 @@
-
-
-def testshow():
-    print ("aaa111")
--- Little_See_Server/little_see/main.py
+++ Little_See_Server/little_see/main.py
@@ -3,4 +3,46 @@
 
 from server.server import runServer
 
-runServer()
+
+
+
+
+
+import time, threading
+from colorama import init,Fore
+
+def getInfoDate():
+    print(Fore.GREEN + 'getInfoDate is running')
+
+    while True:
+        local_time = time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time()))
+        local_hours = time.strftime('%H',time.localtime(time.time()))
+        local_minute = time.strftime('%M',time.localtime(time.time()))
+        print("" , local_hours , ":" , local_minute)
+
+        if local_hours == 6 and local_minute < 30:
+            #每天6点 更新所有知乎日报 好奇心日报 图片 数据
+            pass
+
+        #每小时更新网易新闻数据
+
+
+        time.sleep(60 * 50)
+
+def startServer():
+    print(Fore.GREEN + 'startServer is running')
+    runServer()
+
+
+
+if __name__ == '__main__':
+    init(autoreset=True)
+    print(Fore.RED + 'Server is running')
+
+    thread_getInfoDate = threading.Thread(target=getInfoDate, name='getInfoDate')
+    thread_startServer = threading.Thread(target=startServer, name='startServer')
+    thread_getInfoDate.start()
+    thread_startServer.start()
+    thread_getInfoDate.join()
+    thread_startServer.join()
+
