b51295a500301656474a154c95334105bfd84403
clwater <gengzhibo@gengzhibo.net>
2017-03-16 15:24:17 +0800

    20170316

--- Little_See_Server/LittleSeeServer/server/server_diary.py
+++ Little_See_Server/LittleSeeServer/server/server_diary.py
@@ -12,12 +12,13 @@ def re_data_diary(indexclass):
 
     print(sql)
     cursor.execute(sql)
-    haoqixinlist = cursor.fetchall()
-    json = '{"statu":"haoqixin","date":['
+    diarylist = cursor.fetchall()
+    json = '{"statu":"diary","date":['
 
 
-    for haoqixin in haoqixinlist:
-        json = json + '{ "title":"%s" , "title_image" : "%s" , "address" : "%s"},'%(haoqixin[4] , haoqixin[1] , haoqixin[2])
+    for diary in diarylist:
+        # print(diary)
+        json = json + '{ "title":"%s" , "image" : "%s" , "address" : "%s" , "indexclass" : "%s"},'%(diary[1] , diary[2] , diary[3] , diary[4])
 
     json = json[0:len(json) - 1]
 
