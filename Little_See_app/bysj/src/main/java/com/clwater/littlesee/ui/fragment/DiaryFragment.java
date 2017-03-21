2cc3447ec10ec3aafef3140595fd64763bcebaf7
clwater <gengzhibo@gengzhibo.net>
2017-03-21 16:21:27 +0800

    20170321

--- Little_See_app/bysj/src/main/java/com/clwater/littlesee/ui/fragment/DiaryFragment.java
+++ Little_See_app/bysj/src/main/java/com/clwater/littlesee/ui/fragment/DiaryFragment.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -16,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.clwater.littlesee.R;
@@ -113,7 +111,6 @@ public class DiaryFragment extends Fragment {
         fav.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                //Log.d("gzb" , "" + fav.getTitle());
                 inChooseItemActivity();
                 return false;
             }
