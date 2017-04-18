630b3a34e5c8d17e42d23e9c74c3f40f4684dc68
clwater <gengzhibo@gengzhibo.net>
2017-04-18 22:15:50 +0800

    20170418

--- Little_See_app/bysj/src/main/java/com/clwater/littlesee/ui/fragment/ImageFragment.java
+++ Little_See_app/bysj/src/main/java/com/clwater/littlesee/ui/fragment/ImageFragment.java
@@ -2,6 +2,8 @@ package com.clwater.littlesee.ui.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 
 import com.clwater.littlesee.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,7 +19,11 @@ import butterknife.ButterKnife;
  */
 //实景
 public class ImageFragment extends Fragment {
+    @BindView(R.id.recylist_imagelist) RecyclerView recylist_imagelist;
+    @BindView(R.id.swipecontainer_imagelist) SwipeRefreshLayout swipecontainer_imagelist;
 
+
+    private boolean _getINNewDate = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +32,32 @@ public class ImageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_image, container, false);
         ButterKnife.bind(this , view);
 
+        setHasOptionsMenu(true);
+
+        init();
+
+
+//        getDataFromServer();
+
         return view;
+    }
+
+
+    private void init() {
+        swipecontainer_imagelist.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (_getINNewDate) {
+                    _getINNewDate = false;
+                    getDataFromServer();
+                }
+            }
+        });
+        swipecontainer_imagelist.setColorScheme(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
+    }
+
+    private void getDataFromServer() {
+        swipecontainer_imagelist.setRefreshing(true);
     }
 
 
